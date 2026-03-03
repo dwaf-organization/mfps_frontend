396ac28e574336bf886e9b17bdb0387783d2ffcc
Sutaenghhh <neocop7@gmail.com>
2026-03-03 10:34:48 +0900

    가로모드 , 부위별 욕창케어 입력 그래프4개씩으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/app.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]);
+void main() {
   runApp(const ProviderScope(child: MedicalApp()));
 }
